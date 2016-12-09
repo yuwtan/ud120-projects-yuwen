29437a7cecb5ec610b49655219399ff62e4ec23d
Yuwen Tan <yuwtan@gmail.com>
2016-12-08 21:48:42 -0600

    add rf importance

--- yuwen_ideas/poi_id.py
+++ yuwen_ideas/poi_id.py
@@ -3,127 +3,152 @@
 import sys
 import pickle
 import math
-from sklearn import preprocessing
-from sklearn import metrics
-from sklearn.decomposition import RandomizedPCA
-sys.path.append("../tools/")
+from matplotlib import pyplot
+import warnings
+import numpy as np
 
+warnings.filterwarnings('ignore')
+from sklearn import preprocessing
+
+sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-features_list = ['poi','salary', 'bonus',
-                 'total_payments', 'total_stock_value',
-                 'ratio_from_poi', 'ratio_to_poi',
-                 'ratio_shared_with_poi']
-
-### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Task 2: Remove outliers
+""" scatterplot salary and bonus """
+for person in data_dict:
+    # read person's information
+    data_point = data_dict[person]
+    salary = float(data_point['salary'])
+    bonus = float(data_point['bonus'])
+    shared_receipt_with_poi = float(data_point['shared_receipt_with_poi'])
+    to_messages = float(data_point['to_messages'])
 
-my_dataset = data_dict
-if 'TOTAL' in my_dataset:
-    del my_dataset['TOTAL']
+    # find people with strange email data
+    if shared_receipt_with_poi > to_messages:
+        print "Person with strange email information: \t", person
+        print "shared_receipt_with_poi is: \t", shared_receipt_with_poi
+        print "to_messages is: \t", to_messages
 
-### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
+    # scatterlpot salary and bonus
+    # pyplot.scatter(salary, bonus)
 
-for person in my_dataset:
+# pyplot.xlabel("salary")
+# pyplot.ylabel("bonus")
+# pyplot.show()
+
+""" find out the outlier """
+for person in data_dict:
+    data_point = data_dict[person]
+    if float(data_point['salary']) > 10000000:
+        print "Person with salary higher than 10 million: \t", person
+
+""" remove the outlier """
+data_dict.pop('TOTAL', 0);
+
+""" correct email data """
+data_dict['GLISAN JR BEN F']['shared_receipt_with_poi'] = data_dict['GLISAN JR BEN F']['to_messages']
+
+for person in data_dict:
+
     # read dataset information
-    data_point = my_dataset[person]
+    data_point = data_dict[person]
 
     from_poi_to_this_person = float(data_point['from_poi_to_this_person'])
+    shared_receipt_with_poi = float(data_point['shared_receipt_with_poi'])
     to_messages = float(data_point['to_messages'])
 
     from_this_person_to_poi = float(data_point['from_this_person_to_poi'])
     from_messages = float(data_point['from_messages'])
 
-    # create new features
-    if math.isnan(from_poi_to_this_person) or math.isnan(to_messages) or to_messages == 0:
-        ratio_from_poi = 0
-    else:
-        ratio_from_poi = from_poi_to_this_person / to_messages
-
-    if math.isnan(from_this_person_to_poi) or math.isnan(from_messages) or from_messages == 0:
-        ratio_to_poi = 0
-    else:
-        ratio_to_poi = from_this_person_to_poi / from_messages
-
-    data_point['ratio_from_poi'] = ratio_from_poi
-    data_point['ratio_to_poi'] = ratio_to_poi
-
-for person in my_dataset:
-    # read dataset information
-    data_point = my_dataset[person]
-
-    shared_receipt_with_poi = float(data_point['shared_receipt_with_poi'])
-    to_messages = float(data_point['to_messages'])
-
     total_stock_value = float(data_point['total_stock_value'])
     total_payments = float(data_point['total_payments'])
 
     # create new features
-    if math.isnan(shared_receipt_with_poi) or math.isnan(to_messages) or to_messages == 0:
-        ratio_shared_with_poi = 0
+    if math.isnan(from_poi_to_this_person) or math.isnan(to_messages) or to_messages == 0:
+        ratio_from_poi_to_this_person = 0
     else:
-        ratio_shared_with_poi = shared_receipt_with_poi / to_messages
+        ratio_from_poi_to_this_person = from_poi_to_this_person / to_messages
+
+    if math.isnan(from_this_person_to_poi) or math.isnan(from_messages) or from_messages == 0:
+        ratio_from_this_person_to_poi = 0
+    else:
+        ratio_from_this_person_to_poi = from_this_person_to_poi / from_messages
+
+    if math.isnan(shared_receipt_with_poi) or math.isnan(to_messages) or to_messages == 0:
+        ratio_shared_receipt_with_poi = 0
+    else:
+        ratio_shared_receipt_with_poi = shared_receipt_with_poi / to_messages
 
     if math.isnan(total_stock_value) or math.isnan(total_payments) or total_payments == 0:
         ratio_stock_to_payments = 0
     else:
         ratio_stock_to_payments = total_stock_value / total_payments
 
-    data_point['ratio_shared_with_poi'] = ratio_shared_with_poi
+    # save new features
+    data_point['ratio_from_poi_to_this_person'] = ratio_from_poi_to_this_person
+    data_point['ratio_from_this_person_to_poi'] = ratio_from_this_person_to_poi
+    data_point['ratio_shared_receipt_with_poi'] = ratio_shared_receipt_with_poi
     data_point['ratio_stock_to_payments'] = ratio_stock_to_payments
 
-### Extract features and labels from dataset for local testing
+data_dict1 = {}
+data_dict2 = {}
+for person in data_dict:
+    if math.isnan(float(data_dict[person]['to_messages'])) and \
+        math.isnan(float(data_dict[person]['from_messages'])):
+        data_dict2[person] = data_dict[person]
+    else:
+        data_dict1[person] = data_dict[person]
 
-data = featureFormat(my_dataset, features_list, sort_keys = True)
-labels, features = targetFeatureSplit(data)
+print len(data_dict1)
+print len(data_dict2)
 
-min_max_scaler = preprocessing.MinMaxScaler()
-features = min_max_scaler.fit_transform(features)
+features_list1 = ['poi',
+                 'salary', 'deferral_payments',
+                 'total_payments', 'loan_advances',
+                 'bonus', 'restricted_stock_deferred',
+                 'deferred_income', 'total_stock_value',
+                 'expenses', 'exercised_stock_options',
+                 'other', 'long_term_incentive',
+                 'restricted_stock', 'director_fees',
+                 'to_messages', 'from_poi_to_this_person',
+                 'from_messages', 'from_this_person_to_poi',
+                 'shared_receipt_with_poi',
+                 'ratio_from_poi_to_this_person', 'ratio_from_this_person_to_poi',
+                 'ratio_shared_receipt_with_poi', 'ratio_stock_to_payments'
+                ]
+features_list2 = ['poi',
+                 'salary', 'deferral_payments',
+                 'total_payments', 'loan_advances',
+                 'bonus', 'restricted_stock_deferred',
+                 'deferred_income', 'total_stock_value',
+                 'expenses', 'exercised_stock_options',
+                 'other', 'long_term_incentive',
+                 'restricted_stock', 'director_fees',
+                 'ratio_stock_to_payments'
+                ]
 
-
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-
-from sklearn import svm
-clf = svm.SVC(kernel="rbf", C=10)
-
-pca = RandomizedPCA(n_components=3, whiten=True).fit(features)
-features = pca.transform(features)
-
-
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-# Example starting point. Try investigating other evaluation techniques!
+data1 = featureFormat(data_dict1, features_list1, sort_keys = True)
+data2 = featureFormat(data_dict2, features_list2, sort_keys = True)
+labels, features = targetFeatureSplit(data1)
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
-clf.fit(features_train, labels_train)
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(n_estimators=1000, random_state=0, n_jobs=-1)
+forest.fit(features, labels)
+importances = forest.feature_importances_
+indices = np.argsort(importances)[::-1]
+for f in range(len(features[0])):
+    print("%2d) %-*s %f" % (f+1, 30, features_list1[indices[f]+1], importances[indices[f]]))
 
-labels_pred = clf.predict(features_test)
-print(metrics.classification_report( labels_test, labels_pred ))
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
-
-dump_classifier_and_data(clf, my_dataset, features_list)
+labels, features = targetFeatureSplit(data2)
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+forest.fit(features, labels)
+importances = forest.feature_importances_
+indices = np.argsort(importances)[::-1]
+for f in range(len(features[0])):
+    print("%2d) %-*s %f" % (f+1, 30, features_list2[indices[f]+1], importances[indices[f]]))
